f9397ba7c7b0b944eba7b6f0077661d7a92e4fce
Shrimp <lcamara0916@gmail.com>
2022-03-08 14:14:27 -0100

    Web page can now send the image to the server

--- WEB-PAGE/script.js
+++ WEB-PAGE/script.js
@@ -5,13 +5,17 @@ let uploaded_image;
 let images = [];
 
 input.addEventListener('change', () => {
-    const reader = new FileReader();
-    reader.onload = function () {
-        const img = new Image();
-        img.src = reader.result;
-        images.push(img);
-    }
-    reader.readAsDataURL(input.files[0]);
+    const data = new FormData();
+    data.append('image', input.files[0]);
+
+    fetch('http://localhost:5000/upload', {
+        method: 'POST',
+        body: data,
+    }).then((result) => {
+        console.log("File Sent Successful");
+    }).catch((err) => {
+        console.log(err.message);
+    })
 })
 
 $('#upload').click(() => {
