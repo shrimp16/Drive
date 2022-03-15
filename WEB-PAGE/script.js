6a0cce5902639d17d81d10ffb0bbc13691b1af8e
Shrimp <lcamara0916@gmail.com>
2022-03-15 14:30:55 -0100

    Refactor

--- WEB-PAGE/script.js
+++ WEB-PAGE/script.js
@@ -1,9 +1,7 @@
-import * as card from './src/cards.js';
+import * as uploadManager from './src/upload.js'
 
 let input = document.getElementById('file');
 let body = document.getElementById('body');
-let progressBar = document.getElementById('progress');
-let progressText = document.getElementById('progress-text');
 let progress = document.getElementById('progress-menu');
 
 function cleanBody() {
@@ -27,43 +25,8 @@ function getFiles(number) {
     }
 }
 
-function uploadFiles(data) {
-
-    let dataToSend = new FormData();
-
-    if(data.length > 10){
-        card.show('The file limit is 10', 'lightgray');
-        return
-    }
-
-    for(let i = 0; i < data.length; i++){
-        dataToSend.append('files', data[i]);
-    }
-
-    sendToServer(dataToSend);
-
-}
-
-function sendToServer(files){
-
-    let xhr = new XMLHttpRequest();
-    xhr.open('POST', 'http://192.168.1.103:5000/upload');
-
-    xhr.upload.addEventListener('progress', ({loaded, total}) => {
-        let load = Math.floor((loaded / total) * 100);
-        progressBar.max = 100;
-        progressBar.value = load;
-        progressText.innerText = load + '%';
-        if(loaded === total){
-            progressText.innerText = 'File sent!'
-        }
-    })
-    xhr.send(files);
-
-}
-
 input.addEventListener('change', () => {
-    uploadFiles(input.files);
+    uploadManager.uploadFiles(input.files);
 })
 
 $('#upload').click(() => {
@@ -72,29 +35,8 @@ $('#upload').click(() => {
         <p>Drag your files to here or click to select files</p>
         <i class="fa-solid fa-arrow-up-from-bracket"></i>
     </div>`;
-
     progress.style.display = 'flex';
-
-    let uploadMenu = document.querySelector('#drag');
-
-    uploadMenu.addEventListener('click', () => {
-        $('#file').trigger('click');
-    })
-
-    uploadMenu.addEventListener('dragover', (e) => {
-        e.preventDefault();
-        uploadMenu.classList.add('drag-hover');
-    })
-
-    uploadMenu.addEventListener('dragleave', () => {
-        uploadMenu.classList.remove('drag-hover');
-    })
-
-    uploadMenu.addEventListener('drop', (e) => {
-        e.preventDefault();
-        uploadFiles(e.dataTransfer.files);
-    })
-
+    uploadManager.setupDragAndDrop();
 })
 
 $('#files').click(() => {
---
+++ WEB-PAGE/src/upload.js
@@ -0,0 +1,62 @@
+import * as card from './cards.js';
+
+let progressBar = document.getElementById('progress');
+let progressText = document.getElementById('progress-text');
+
+export function setupDragAndDrop() {
+    let uploadMenu = document.querySelector('#drag');
+
+    uploadMenu.addEventListener('click', () => {
+        $('#file').trigger('click');
+    })
+
+    uploadMenu.addEventListener('dragover', (e) => {
+        e.preventDefault();
+        uploadMenu.classList.add('drag-hover');
+    })
+
+    uploadMenu.addEventListener('dragleave', () => {
+        uploadMenu.classList.remove('drag-hover');
+    })
+
+    uploadMenu.addEventListener('drop', (e) => {
+        e.preventDefault();
+        uploadFiles(e.dataTransfer.files);
+    })
+
+}
+
+export function uploadFiles(data) {
+
+    let dataToSend = new FormData();
+
+    if(data.length > 10){
+        card.show('The file limit is 10', 'lightgray');
+        return
+    }
+
+    for(let i = 0; i < data.length; i++){
+        dataToSend.append('files', data[i]);
+    }
+
+    sendToServer(dataToSend);
+
+}
+
+function sendToServer(files){
+
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', 'http://192.168.1.103:5000/upload');
+
+    xhr.upload.addEventListener('progress', ({loaded, total}) => {
+        let load = Math.floor((loaded / total) * 100);
+        progressBar.max = 100;
+        progressBar.value = load;
+        progressText.innerText = load + '%';
+        if(loaded === total){
+            progressText.innerText = 'File sent!'
+        }
+    })
+    xhr.send(files);
+
+}
