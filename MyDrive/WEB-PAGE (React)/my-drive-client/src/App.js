e2f5c96a7d2abad7c28dc269dc43f237bb3cb43a
Shrimp <lcamara0916@gmail.com>
2022-07-13 20:10:38 +0000

    Fixed problem with dependecies

--- MyDrive/WEB-PAGE (React)/my-drive-client/src/App.js
+++ MyDrive/WEB-PAGE (React)/my-drive-client/src/App.js
@@ -1,9 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import { MainTitle } from './Styles/Titles.styled';
+
 function App() {
   return (
     <>
       <Router>
+        <MainTitle>My Drive</MainTitle>
         <Routes>
           <Route path='/'></Route>
           <Route path='/register'></Route>
