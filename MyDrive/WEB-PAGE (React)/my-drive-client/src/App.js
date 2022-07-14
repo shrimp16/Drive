8c6bd344352aca8e108df7082b42bbd99eb3b7d7
Shrimp <lcamara0916@gmail.com>
2022-07-14 10:03:42 +0000

    Implemented the login UI

--- MyDrive/WEB-PAGE (React)/my-drive-client/src/App.js
+++ MyDrive/WEB-PAGE (React)/my-drive-client/src/App.js
@@ -1,15 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import { StyledMainHeader } from './Styles/Titles.styled';
+
 import Register from './Components/Register';
+import Login from './Components/Login';
 
 function App() {
   return (
     <>
       <Router>
+        <StyledMainHeader>My Drive</StyledMainHeader>
         <Routes>
           <Route path='/'></Route>
           <Route path='/register' element={<Register />}></Route>
-          <Route path='/login'></Route>
+          <Route path='/login' element={<Login />}></Route>
         </Routes>
       </Router>
     </>
--- MyDrive/WEB-PAGE (React)/my-drive-client/src/Styles/AuthContainer.styled.js
+++ MyDrive/WEB-PAGE (React)/my-drive-client/src/Styles/AuthContainer.styled.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const StyledAuthContainer = styled.div`
     display: flex;
     flex-direction: column;
-    margin-top: 20vh;
+    margin-top: 18vh;
 
     p {
         margin: auto;
