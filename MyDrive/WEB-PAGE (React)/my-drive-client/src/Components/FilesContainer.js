b58e83f61c20986b852a0f929b738ef0e38c8553
Shrimp <lcamara0916@gmail.com>
2022-07-16 15:11:05 +0000

    Added multiple dummy image elements to try out styles

--- MyDrive/WEB-PAGE (React)/my-drive-client/src/Components/FilesContainer.js
+++ MyDrive/WEB-PAGE (React)/my-drive-client/src/Components/FilesContainer.js
@@ -1,11 +1,33 @@
 import { StyledFilesContainer } from '../Styles/FilesContainer.styled';
+import image from '../Images/test-image.jpg';
+
+import { StyledImage } from '../Styles/Image.styled';
 
 function FilesContainer() {
 
     return (
         <>
             <StyledFilesContainer height={window.innerHeight}>
-                
+                <StyledImage src={image} alt='test'></StyledImage>
+                <StyledImage src={image} alt='test'></StyledImage>
+                <StyledImage src={image} alt='test'></StyledImage>
+                <StyledImage src={image} alt='test'></StyledImage>
+                <StyledImage src={image} alt='test'></StyledImage>
+                <StyledImage src={image} alt='test'></StyledImage>
+                <StyledImage src={image} alt='test'></StyledImage>
+                <StyledImage src={image} alt='test'></StyledImage>
+                <StyledImage src={image} alt='test'></StyledImage>
+                <StyledImage src={image} alt='test'></StyledImage>
+                <StyledImage src={image} alt='test'></StyledImage>
+                <StyledImage src={image} alt='test'></StyledImage>
+                <StyledImage src={image} alt='test'></StyledImage>
+                <StyledImage src={image} alt='test'></StyledImage>
+                <StyledImage src={image} alt='test'></StyledImage>
+                <StyledImage src={image} alt='test'></StyledImage>
+                <StyledImage src={image} alt='test'></StyledImage>
+                <StyledImage src={image} alt='test'></StyledImage>
+                <StyledImage src={image} alt='test'></StyledImage>
+                <StyledImage src={image} alt='test'></StyledImage>
             </StyledFilesContainer>
         </>
     )
---
+++ MyDrive/WEB-PAGE (React)/my-drive-client/src/Styles/Image.styled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const StyledImage = styled.img`
+    height: 13rem;
+    width: 13rem;
+    aspect-ratio: 1/1;
+`
